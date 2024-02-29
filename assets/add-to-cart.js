d71747971f53bfb80ec3cf656120bd88b2c081e7
Arulmurugan <arulmurugan11s2k@gmail.com>
2024-02-29 11:19:11 +0530

    added inventory bar

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,39 +1,36 @@
-const btn = document.getElementById('custom-my-btn');
-const firstNameInput = document.getElementById('firstNameInput');
-
-const cartQnty = document.querySelector(".quantity__input").getAttribute('data-cart-quantity');
-console.log(cartQnty);
 
 
-btn.addEventListener('click', () => {
-  // Get user input from the input field
-  const userFirstName = firstNameInput.value;
 
-  let formData = {
-    items: [
-      {
-        id: btn.dataset.productId,
-        quantity: btn.dataset.quantity,
-        properties: {
-          "First name": userFirstName,
-          "_Last Name": "Murugan"
-        },
-      },
-    ],
-  };
+// btn.addEventListener("click", () => {
+//   const cart = document.querySelector("cart-drawer");
+//   console.log(cart.getSectionsToRender());
+//   let formData = {
+//     items: [
+//       {
+//         id: btn.dataset.id,
+//         quantity: btn.dataset.quantity,
+//         properties: {
+//           "First name": userInput.value,
+//           "_Last Name": "Hari",
+//         },
+//         sections: cart.getSectionsToRender().map((section) => section.id),
+//       },
+//     ],
+//   };
 
-  fetch("/cart/add.js", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
+//   fetch("/cart/add.js", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   })
+//     .then((response) => {
+//       console.log(response);
+//       cart.open();
+//       return response.json();
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// });
--- assets/global.js
+++ assets/global.js
@@ -1584,4 +1584,48 @@ function copyCode() {
 }
 
 
-/*-----------------------------------------product-voucher----------------------------------------------- */
+
+/*-------------------------------------custom-atc-------------------------------------------------------*/
+
+
+const btn = document.getElementById('custom-my-btn');
+const firstNameInput = document.getElementById('firstNameInput');
+
+const cartQnty = document.querySelector(".quantity__input").getAttribute('data-cart-quantity');
+console.log(cartQnty);
+
+
+btn.addEventListener('click', () => {
+  const cart = document.querySelector("cart-drawer");
+  const userFirstName = firstNameInput.value;
+
+  let formData = {
+    items: [
+      {
+        id: btn.dataset.productId,
+        quantity: btn.dataset.quantity,
+        properties: {
+          "First name": userFirstName,
+          "_Last Name": "Murugan"
+        },
+        sections: cart.getSectionsToRender().map((section) => section.id),
+      },
+    ],
+  };
+
+  fetch("/cart/add.js", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      console.log(response);
+      cart.open();
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
